992979a8e8dc4b544d96bb1f36597a82993343de
rasmi142 <rasmioff@gmail.com>
2025-12-22 21:58:58 +0530

    checklist redrafting and No Need option implementation

--- src/models/ChecklistItem.js
+++ src/models/ChecklistItem.js
@@ -2,32 +2,36 @@ const mongoose = require('mongoose');
 const { Schema, Types } = mongoose;
 
 const ChecklistItemSchema = new Schema({
-  engagement: { 
-    type: Types.ObjectId, 
-    ref: 'Engagement', 
+  engagement: {
+    type: Types.ObjectId,
+    ref: 'Engagement',
     required: true,
-    index: true 
+    index: true
   },
-  key: { 
-    type: String, 
-    required: true, 
-    index: true 
+  key: {
+    type: String,
+    required: true,
+    index: true
   },
-  description: { 
-    type: String, 
-    required: true 
+  description: {
+    type: String,
+    required: true
   },
-  category: { 
-    type: String, 
-    required: true 
+  category: {
+    type: String,
+    required: true
   },
   subcategory: {
     type: String,
     required: true
   },
-  completed: { 
-    type: Boolean, 
-    default: false 
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  isNotApplicable: {
+    type: Boolean,
+    default: false
   },
   fieldType: {
     type: String,
